3d27bb68df50e8d25572f206c4e7c79d1f7da15c
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-02-02 14:41:26 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var exec = require('gulp-exec');
+
+module.exports = function(opt) {
+  var cmd = 'php ./ngn-env/run/run.php "new CliAccessArgsSingle(\'html <%= file.path %> ' + //
+    opt.buildFolder + ' ' + opt.name + '\', new CssBuildTool)" ngn-cs';
+  return exec(cmd, {
+    continueOnError: false,
+    pipeStdout: true
+  });
+};
+module.exports.reporter = exec.reporter;
+
+

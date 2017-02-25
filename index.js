655f9c2ea6409131677532b85ca6cad590e13916
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-02-25 10:31:43 +0300

    v1.0.3

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-var exec = require('gulp-exec');
+var gulp = require('gulp');
+var through2 = require('through2');
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
 
-module.exports = function(opt) {
-  var cmd = 'php ' + //
-    (opt.ngnEnvFolder || './ngn-env') + //
-    '/run/run.php "new CliAccessArgsSingle(\'html <%= file.path %> ' + //
-    opt.buildFolder + ' ' + opt.name + '\', new CssBuildTool)" ngn-cs';
-  return exec(cmd, {
-    continueOnError: false,
-    pipeStdout: true
-  });
+module.exports = function (opt) {
+  return through2.obj(function (file, enc, cb) {
+    var that = this;
+    // --------------------- PHP NGN COMMAND -----------------------------
+    var cmd = 'php ' + //
+      (opt.ngnEnvFolder || './ngn-env') + //
+      '/run/run.php "new CliAccessArgsSingle(\'html <%= file.path %> ' + //
+      opt.buildFolder + ' ' + opt.name + '\', new CssBuildTool)" ngn-cs' +
+      (opt.projectFolder ? ',' + opt.projectFolder : '');
+    // -------------------------------------------------------------------
+    exec(cmd,
+      function (err, stdout) {
+        if (err) {
+          that.emit('error', new gutil.PluginError('ngn', stdout));
+          that.push(file);
+        } else {
+          stdout = stdout.trim();
+          var name = file.path.replace(/(.*)\/(.*)\.json/g, "$2");
+          file.path = file.path.replace(/(.*)\.json/g, "$1.js");
+          file.contents = new Buffer(stdout);
+          gutil.log(stdout);
+          that.push(file);
+        }
+        cb();
+      });
+  })
 };
-module.exports.reporter = exec.reporter;
 
-
+module.exports.reporter = function() {
+   // dummy for backwork compability
+	return through2.obj(function (file, enc, cb){
+		this.push(file);
+		cb();
+	});
+}
